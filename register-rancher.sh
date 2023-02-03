3e1a66b02b98a9d3471849e2c07a0b0bfade66e6
Niel M. Bornstein <nbornstein@suse.com>
2023-02-03 17:18:23 -0500

    Made it actually work

--- register-rancher.sh
+++ register-rancher.sh
@@ -1,32 +1,48 @@
 #!/bin/bash
 
-RANCHER_URL=https://mmrancher3.houseofswartz.net
+# Rancher API endpoint
+RANCHER_API_ENDPOINT=https://mmrancher3.houseofswartz.net
 
-# Create imported Cluster and get the cluster ID
-#TOKEN={token-RancherAPIToken}
-TOKEN=token-2v98m:xmppsccz58lrdnh8sfwq75hx67dvbfm6qgdgm8ghmkkcqf5gt82tkd
-CLUSTER_ID=$(curl -k $RANCHER_URL/v3/clusters \
-  -H "content-type: application/json" \
-  -H "Authorization: Bearer $TOKEN" \
-  --data-binary '{
-    "type": "cluster",
-    "importedConfig": {
-    },
-    "name": "slb-k3s"
-  }' | jq -r .id)
-echo $CLUSTER_ID
-  
-# Run the kubectl command to register Cluster to Rancher
-k3sregister() {
-CLUSTER_CONNECT=$(curl -k $RANCHER_URL/v3/clusters/$CLUSTER_ID/clusterregistrationtokens \
-  -H "content-type: application/json" \
-  -H "Authorization: Bearer $TOKEN" \
-  | jq -r .data[0].insecureCommand)
-#echo $CLUSTER_CONNECT
-eval "$CLUSTER_CONNECT"
-}
+# Rancher API access key and secret key
+RANCHER_ACCESS_KEY=token-thj25
+RANCHER_SECRET_KEY=fkcjk5dhskvxk65q5bdwdnv7n8bfbzjz59d7nxb8zsfvn2l9fh8vl7
 
-# Register cluster
-k3sregister
+# Kubernetes cluster name
+CLUSTER_NAME=niel
+
+# Create a new cluster in Rancher using the API and get the ID
+cluster_id=$(curl -ks \
+  "${RANCHER_API_ENDPOINT}/v3/cluster" \
+  -H "Authorization: Bearer $RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" \
+  -H "Content-Type: application/json" \
+  -d '{
+  "type": "cluster",
+  "name": "'"$CLUSTER_NAME"'",
+  "importedConfig": {}
+}' | jq -r '.id')
+
+echo $cluster_id
+
+# Get the registration token from Rancher using the API
+registration_token=$(curl -ks \
+  "${RANCHER_API_ENDPOINT}/v3/clusterregistrationtoken" \
+  -H "Authorization: Bearer $RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" \
+  -H "Content-Type: application/json" \
+  -d '{
+  "type": "clusterRegistrationToken",
+  "clusterId": "'"$cluster_id"'"
+}' | jq -r '.id')
+
+echo $registration_token
+
+# Get the registration command line from Rancher using the API
+registration_command=$(curl -ks \
+  "${RANCHER_API_ENDPOINT}/v3/clusterregistrationtokens/$registration_token" \
+  -H "Authorization: Bearer $RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" \
+  | jq -r '.insecureCommand')
+
+# Print the registration command line
+echo $registration_command
+eval $registration_command
 
 touch /tmp/rancher-registered
